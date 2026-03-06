a4dde78d35db9e6d8496ef31984464a32df03f86
Ebubeker <rexhaebubeker@gmail.com>
2026-03-06 01:00:07 +0100

    fix image size

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '100mb',
+    },
+  },
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_APP_SUPABASE_URI,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_APP_SUPABASE_ANON,
